ca40c89cda634b26bdb3a4d584b62ff9992b0dd8
Nesokas <ines.moital@gmail.com>
2013-03-09 23:09:21 +0000

    player movement with keyboard completed

--- Assets/Scripts/PlayerBehaviour.js
+++ Assets/Scripts/PlayerBehaviour.js
@@ -1,49 +1,96 @@
 #pragma strict
 
-var speed = 0.001;
-var player_max_speed = 0.5;
+var speed = 1;
+var decrease_speed = 0.5;
+var player_max_speed = 10;
 
 // Directions Vectors
-var left = Vector3(0.0, 0.0, -1.0);
-var right = Vector3(0.0, 0.0, 1.0);
-var up = Vector3(1.0, 0.0, 0.0);
-var down = Vector3(-1.0, 0.0, 0.0);
+var left_direction = Vector3(0.0, 0.0, 1.0);
+var right_direction = Vector3(0.0, 0.0, -1.0);
+var up_direction = Vector3(1.0, 0.0, 0.0);
+var down_direction = Vector3(-1.0, 0.0, 0.0);
 
-function increaseSpeed(direction) {
+// Movement keys pressed?
+var up_keyUp = true; 
+var down_keyUp = true; 
+var right_keyUp = true;
+var left_keyUp = true;
+
+function horizontal_velocity() 
+{
+	return rigidbody.velocity.z;
+}
+
+function vertical_velocity()
+{
+	return rigidbody.velocity.x;
+}
+
+function increaseSpeed(direction) 
+{
 	switch (direction) {
 	case "right":
-		if(rigidbody.velocity.z >= -player_max_speed)
-			rigidbody.velocity += left * speed;
+		if(horizontal_velocity() > -player_max_speed)
+			rigidbody.velocity += right_direction * speed;
 		break;
 	case "left":
-		if(rigidbody.velocity.z <= player_max_speed)
-			rigidbody.velocity -= right * speed;
+		if(horizontal_velocity() < player_max_speed) {
+			rigidbody.velocity += left_direction * speed;
+			Debug.Log(rigidbody.velocity);
+		}
 		break;
 	case "up":
-		if(rigidbody.velocity.x <= player_max_speed)
-			rigidbody.velocity += up*speed;
+		if(vertical_velocity() < player_max_speed)
+			rigidbody.velocity += up_direction * speed;
 		break;
 	case "down":
-		if(rigidbody.velocity.x >= -player_max_speed)
-			rigidbody.velocity += down * speed;
+		if(vertical_velocity() > -player_max_speed)
+			rigidbody.velocity += down_direction * speed;
 	}
 }
 
-function Start () {
-
+function decreaseVelocity() 
+{
+	if(right_keyUp && horizontal_velocity() < 0)
+		rigidbody.velocity -= right_direction * decrease_speed;
+	if(left_keyUp && horizontal_velocity() > 0)
+		rigidbody.velocity -= left_direction * decrease_speed;
+		
+	if(down_keyUp && vertical_velocity() < 0)
+		rigidbody.velocity -= down_direction * decrease_speed;
+	if(up_keyUp && vertical_velocity() > 0)
+		rigidbody.velocity -= up_direction * decrease_speed;
 }
+
+function Start () {}
 
 function Update () 
 {
-	if(Input.GetKey("a"))
+	if(Input.GetKey("a")) {
 		increaseSpeed("left");
-	
-	if(Input.GetKey("d"))
+		left_keyUp = false;
+	}
+	if(Input.GetKey("d")) {
 		increaseSpeed("right");
-		
-	if(Input.GetKey("w"))
+		right_keyUp = false;
+	}
+	if(Input.GetKey("w")) {
 		increaseSpeed("up");
-	
-	if(Input.GetKey("s"))
+		up_keyUp = false;
+	}
+	if(Input.GetKey("s")) {
 		increaseSpeed("down");
+		down_keyUp = false;
+	}
+	
+	if(Input.GetKeyUp("a"))
+		left_keyUp = true;
+	if(Input.GetKeyUp("d"))
+		right_keyUp = true;
+	if(Input.GetKeyUp("w"))
+		up_keyUp = true;
+	if(Input.GetKeyUp("s"))
+		down_keyUp = true;
+	
+	decreaseVelocity();
 }
