7f800cff7f1e44c736b398adc16ad81c4c71b967
Nuno Alexandre Pinto da Costa <nuno_costa@live.com>
2013-03-17 00:14:55 +0000

    Player movement is now correct

--- Assets/Scripts/PlayerBehaviour.js
+++ Assets/Scripts/PlayerBehaviour.js
@@ -1,15 +1,15 @@
 #pragma strict
 
-var speed = 0.5;
-var decrease_speed = 0.2;
-var player_max_speed = 3;
+var acceleration = 0.2;
+var player_max_speed = 3.5;
 
 // Directions Vectors
 var left_direction = Vector3(0.0, 0.0, 1.0);
 var right_direction = Vector3(0.0, 0.0, -1.0);
 var up_direction = Vector3(1.0, 0.0, 0.0);
 var down_direction = Vector3(-1.0, 0.0, 0.0);
-
+var velocity = Vector3.zero;
+var normalized_velocity = Vector3.zero;
 // Movement keys pressed?
 var up_keyUp = true; 
 var down_keyUp = true; 
@@ -26,48 +26,40 @@ function vertical_velocity()
 	return rigidbody.velocity.x;
 }
 
-function increaseSpeed(direction) 
+function increase_speed(direction) 
 {
 	switch (direction) {
 	case "right":
 		if(horizontal_velocity() > -player_max_speed)
-			rigidbody.velocity += right_direction * speed;
+			rigidbody.velocity += right_direction * acceleration;
 		break;
 	case "left":
 		if(horizontal_velocity() < player_max_speed)
-			rigidbody.velocity += left_direction * speed;
+			rigidbody.velocity += left_direction * acceleration;
 		break;
 	case "up":
 		if(vertical_velocity() < player_max_speed)
-			rigidbody.velocity += up_direction * speed;
+			rigidbody.velocity += up_direction * acceleration;
 		break;
 	case "down":
 		if(vertical_velocity() > -player_max_speed)
-			rigidbody.velocity += down_direction * speed;
+			rigidbody.velocity += down_direction * acceleration;
 	}
+
+}
+
+function normalize_velocity()
+{
+	var degree = Mathf.Atan2(Mathf.Abs(horizontal_velocity()), 
+							 Mathf.Abs(vertical_velocity()));
+	normalized_velocity.x = rigidbody.velocity.x * Mathf.Cos(degree);
+	normalized_velocity.z = rigidbody.velocity.z * Mathf.Sin(degree); 
 }
 
 function Start () {}
 
 function Update () 
 {
-	if(Input.GetKey("a")) {
-		increaseSpeed("left");
-		left_keyUp = false;
-	}
-	if(Input.GetKey("d")) {
-		increaseSpeed("right");
-		right_keyUp = false;
-	}
-	if(Input.GetKey("w")) {
-		increaseSpeed("up");
-		up_keyUp = false;
-	}
-	if(Input.GetKey("s")) {
-		increaseSpeed("down");
-		down_keyUp = false;
-	}
-	
 	if(Input.GetKeyUp("a"))
 		left_keyUp = true;
 	if(Input.GetKeyUp("d"))
@@ -76,4 +68,37 @@ function Update ()
 		up_keyUp = true;
 	if(Input.GetKeyUp("s"))
 		down_keyUp = true;
+	
+	if (!up_keyUp || !down_keyUp)
+		rigidbody.velocity.x = velocity.x;
+		
+	if (!left_keyUp || !right_keyUp)
+		rigidbody.velocity.z = velocity.z;
+		
+	if(Input.GetKey("a")) {
+		increase_speed("left");
+		left_keyUp = false;
+	}
+	if(Input.GetKey("d")) {
+		increase_speed("right");
+		right_keyUp = false;
+	}
+	if(Input.GetKey("w")) {
+		increase_speed("up");
+		up_keyUp = false;
+	}
+	if(Input.GetKey("s")) {
+		increase_speed("down");
+		down_keyUp = false;
+	}
+	
+	//if(rigidbody.velocity.magnitude > player_max_speed)
+	
+	normalize_velocity();
+	velocity = rigidbody.velocity;
+	if (!up_keyUp || !down_keyUp)
+		rigidbody.velocity.x = normalized_velocity.x;
+	if (!left_keyUp || !right_keyUp)
+		rigidbody.velocity.z = normalized_velocity.z;
+
 }
