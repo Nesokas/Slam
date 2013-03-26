7ef5288f13c42ca9a9a770ca21858d228f668c39
Nesokas <ines.moital@gmail.com>
2013-03-26 20:37:20 +0000

    Better velocity normalization

--- Assets/Scripts/PlayerBehaviour.js
+++ Assets/Scripts/PlayerBehaviour.js
@@ -2,11 +2,13 @@
 
 var acceleration = 0.3;
 var player_max_speed = 4;
-var shootVelocity = 3.0;
+var shootVelocity = 5.0;
 
 var velocity = Vector3.zero;
 var normalized_velocity = Vector3.zero;
 var gamepad = false;
+
+private var direction = Vector3.zero;
 
 function OnCollisionEnter(collision : Collision)
 {
@@ -32,48 +34,21 @@ function OnCollisionStay(collision : Collision)
 	}
 }
 
-function horizontal_velocity() 
-{
-	return rigidbody.velocity.z;
-}
-
-function vertical_velocity()
-{
-	return rigidbody.velocity.x;
-}
-
 function increase_speed() 
 {
 	var vertical_speed : float;
 	var horizontal_speed : float;
 	if(gamepad){
-		vertical_speed = Input.GetAxis("Vertical_Gamepad")*acceleration;
-		 horizontal_speed = Input.GetAxis("Horizontal_Gamepad")*acceleration;
+		direction.x = Input.GetAxis("Vertical_Gamepad");
+		direction.z = Input.GetAxis("Horizontal_Gamepad");
 	} else {
-		vertical_speed = Input.GetAxis("Vertical")*acceleration;
-		horizontal_speed = Input.GetAxis("Horizontal")*acceleration;
+		direction.x = Input.GetAxis("Vertical");
+		direction.z = Input.GetAxis("Horizontal");
 	}
-	
-	rigidbody.velocity.z -= horizontal_speed;
-	rigidbody.velocity.x += vertical_speed;
-	
-	if(rigidbody.velocity.x < -player_max_speed)
-		rigidbody.velocity.x = -player_max_speed;
-	else if (rigidbody.velocity.x > player_max_speed)
-		rigidbody.velocity.x = player_max_speed;
+	direction.Normalize();
 		
-	if(rigidbody.velocity.z < -player_max_speed)
-		rigidbody.velocity.z = -player_max_speed;
-	else if (rigidbody.velocity.z > player_max_speed)
-		rigidbody.velocity.z = player_max_speed;
-}
-
-function normalize_velocity()
-{
-	var degree = Mathf.Atan2(Mathf.Abs(horizontal_velocity()), 
-							 Mathf.Abs(vertical_velocity()));
-	normalized_velocity.x = rigidbody.velocity.x * Mathf.Cos(degree);
-	normalized_velocity.z = rigidbody.velocity.z * Mathf.Sin(degree); 
+	rigidbody.velocity += direction*acceleration;
+	Debug.Log(rigidbody.velocity);
 }
 
 function Start () {
@@ -87,23 +62,5 @@ function Start () {
 
 function Update () 
 {
-
-	if (Input.GetAxis("Vertical")!=0)
-		rigidbody.velocity.x = velocity.x;
-		
-	if (Input.GetAxis("Horizontal")!=0)
-		rigidbody.velocity.z = velocity.z;
-		
 	increase_speed();
-	
-	normalize_velocity();
-	velocity = rigidbody.velocity;
-	if (Input.GetAxis("Vertical")!=0)
-		rigidbody.velocity.x = normalized_velocity.x;
-		
-	if (Input.GetAxis("Horizontal")!=0)
-		rigidbody.velocity.z = normalized_velocity.z;
-		
-	//Debug.Log(rigidbody.velocity);
-
 }
