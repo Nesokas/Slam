62f6705eeda94cff917f166d4f4fb7f97414be83
Nesokas <ines.moital@gmail.com>
2013-04-14 18:45:18 +0100

    Better lobby area

--- Assets/Scripts/Lobby.js
+++ Assets/Scripts/Lobby.js
@@ -1,62 +1,149 @@
 #pragma strict
 
-var title_style : GUIStyle;
-var title_position : Rect;
-var team_1_style : GUIStyle;
-var team_2_style : GUIStyle;
-var string_to_edit = "Player 1";
-var controllers : String[];
+var lobby_style : GUIStyle;
+var team1_style : GUIStyle;
+var team2_style : GUIStyle;
+var names_style : GUIStyle;
+var height_scale = 0.04;
+var scale = 0.0025;
+var names_scale = 0.04;
 
-private var num_players = 2;
+private var num_controllers = 0;
+private var buttons_scale_width : float;
+
 private var players_team_1 = new Array();
 private var players_team_2 = new Array();
-private var selected_controller = new Array();
-private var editing = new Array();
-private var players_name = new Array();
-private var editing_someone = false;
+private var players_team_0 = new Array();
+private var players_first_loggin = new Array();
 
 function Start()
 {
-	players_team_1[0] = 0;
-	players_team_2[0] = 1;
-	for(var i = 0; i < num_players; i++) {
-		selected_controller[i] = "";
-		editing[i] = false;
-		players_name[i] = "";
+	for(var i = 0; i < 5; i++) {
+		players_first_loggin[i] = true;
 	}
 }
 
-private function AddPlayer (position : Vector2, player_num)
-{		
-		players_name[player_num] = GUI.TextField(Rect(position.x, position.y, 100, 20), players_name[player_num], 8);
-		if(GUI.Button(Rect(position.x + 120, position.y, 90, 20), selected_controller[player_num] as String) && !editing_someone) {
-			editing[player_num] = true;
-			editing_someone = true;
+function AddPlayer(position : Vector2, player_name : String, team : int)
+{
+	buttons_scale_width = Screen.height * scale;
+	names_style.fontSize = Screen.height * names_scale;
+	
+	if(team == 1){
+		position.x = Screen.width/6 + Screen.width/25;
+	} else if(team == 2) {
+		position.x = 5*Screen.width/6 - Screen.width/25;
+	}
+	
+	if(team == 2 || team == 0){
+		if(GUI.Button(Rect(position.x - 55*buttons_scale_width, 
+					       position.y + 5*buttons_scale_width,
+					       20*buttons_scale_width,
+					       20*buttons_scale_width), 
+				      "<")) {
+			if(removeFromTeam(player_name, 2))
+				players_team_0.push(player_name);
+			else if(removeFromTeam(player_name, 0))
+				players_team_1.push(player_name);
 		}
+	}
+			   
+	GUI.Box(Rect(position.x - 30*buttons_scale_width,
+		 	     position.y + 1*buttons_scale_width,
+				 60*buttons_scale_width,
+				 30*buttons_scale_width), 
+			player_name, 
+			names_style);
+	
+	if(team == 1 || team == 0) {
+		if(GUI.Button(Rect(position.x + 35*buttons_scale_width, 
+						position.y + 5*buttons_scale_width,
+						20*buttons_scale_width,
+						20*buttons_scale_width), ">")) {
+			if(removeFromTeam(player_name, 1))
+				players_team_0.push(player_name);
+			else if(removeFromTeam(player_name, 0))
+				players_team_2.push(player_name);
+		}
+	}
+}
 
-		if(editing[player_num]) {
-			for (var i = 0; i < controllers.Length; i++) {
-				if(GUI.Button(Rect(position.x+120, position.y + 20*(i+1), 90, 20), controllers[i] as String)) {
-					selected_controller[player_num] = controllers[i];
-					editing[player_num] = false;
-					editing_someone = false;
-				}
-			}
+function removeFromTeam(player_name : String, team : int)
+{
+	var team_players : Array;
+	if(team == 1)
+		team_players = players_team_1;
+	else if (team == 2)
+		team_players = players_team_2;
+	else
+		team_players = players_team_0;
+		
+	for(var i = 0; i < team_players.length; i++) {
+		if(player_name == team_players[i]) {
+			team_players.splice(i,1);
+			return true;
 		}
+	}
+	
+	return false;
+}
+
+function isPlayerTeam(player_name : String, team : int)
+{
+	var team_players : Array;
+	if(team == 1)
+		team_players = players_team_1;
+	else
+		team_players = players_team_2;
+		
+	for(var i = 0; i < team_players.length; i++) {
+		if(player_name == team_players[i])
+			return true;
+	}
+	
+	return false;
 }
 
 function OnGUI () {
-
-	GUI.Box (Rect (10,10,Screen.width-20,Screen.height-20), "");
-	GUI.Label(title_position, "Lobby", title_style);
-	GUI.Label(Rect(30, 70, 0, 0), "Team 1", team_1_style);
-	GUI.Label(Rect(Screen.width-50, 70, 0, 0), "Team 2", team_2_style);
 	
-	var i : int;
+	var font_scale = height_scale * Screen.height;
+	lobby_style.fontSize = font_scale;
+	team1_style.fontSize = font_scale;
+	team2_style.fontSize = font_scale;
 	
-	for (i = 0; i < players_team_1.length; i++)
-		AddPlayer(Vector2(30, 100 + i*20), players_team_1[i]);
+	GUI.Box(Rect(10, 10, Screen.width-20, Screen.height-20), "LOBBY", lobby_style); 
+	GUI.Box(Rect(Screen.width/4 - Screen.width/6, 40, Screen.width/4, Screen.height-60), "TEAM 1", team1_style);
+	GUI.Box(Rect(3*Screen.width/4 - Screen.width/12, 40, Screen.width/4, Screen.height-60), "TEAM 2", team2_style);
 	
-	for (i = 0; i < players_team_2.length; i++)
-		AddPlayer(Vector2(Screen.width-230, 100 + i*20), players_team_2[i]);
+	if(players_first_loggin[0]) {
+		players_first_loggin[0] = false;
+		players_team_0.push("Player 0");	
+	}
+	
+	if(isPlayerTeam("Player 0", 1))
+		AddPlayer(Vector2(Screen.width/2,Screen.height/6), "Player 0", 1);
+	else if (isPlayerTeam("Player 0", 2))
+		AddPlayer(Vector2(Screen.width/2,Screen.height/6), "Player 0", 2);
+	else
+		AddPlayer(Vector2(Screen.width/2,Screen.height/6), "Player 0", 0);
+	
+	if(num_controllers < Input.GetJoystickNames().length)
+		num_controllers = Input.GetJoystickNames().length;
+	
+	for(var i = 0; i < num_controllers; i++) {
+		
+		var player_name = "Player " + (i+1);
+		
+		if(players_first_loggin[i+1]) {
+			players_first_loggin[i+1] = false;
+			players_team_0.push(player_name);	
+		}
+		
+		if(isPlayerTeam(player_name, 1))
+			AddPlayer(Vector2(Screen.width/2,Screen.height/6 + 35*buttons_scale_width*(i+1)), player_name, 1);
+		else if (isPlayerTeam(player_name, 2))
+			AddPlayer(Vector2(Screen.width/2,Screen.height/6 + 35*buttons_scale_width*(i+1)), player_name, 2);
+		else
+			AddPlayer(Vector2(Screen.width/2,Screen.height/6 + 35*buttons_scale_width*(i+1)), player_name, 0);
+	}
+	
 }
