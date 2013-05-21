e43cc9dda9ca56ceb5b75e3336faa1b6462162d5
ines <ines.moital@sherpany.com>
2013-05-22 00:37:32 +0100

    Changed score color

--- Assets/Scripts/Screen_Text_Behaviour.cs
+++ Assets/Scripts/Screen_Text_Behaviour.cs
@@ -12,6 +12,9 @@ public class Screen_Text_Behaviour : MonoBehaviour {
 	public Material team1_color;
 	public Material team2_color;
 	
+	public GameObject score_red;
+	public GameObject score_blue;
+	
 	private int score_team_1 = 0;
 	private int score_team_2 = 0;
 	
@@ -79,6 +82,8 @@ public class Screen_Text_Behaviour : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		score_red.renderer.material.color = team1_color.color;
+		score_blue.renderer.material.color = team2_color.color;
 		game_behaviour = main_game_object.GetComponent<Game_Behaviour>();
 	}
 	
@@ -139,8 +144,11 @@ public class Screen_Text_Behaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		TextMesh score = screen_score.GetComponent<TextMesh>();
-		score.text = score_team_1 + "-" + score_team_2;
+		TextMesh text_score_team_1 = score_red.GetComponent<TextMesh>();
+		TextMesh text_score_team_2 = score_blue.GetComponent<TextMesh>();
+		
+		text_score_team_1.text = score_team_1.ToString();
+		text_score_team_2.text = score_team_2.ToString();
 		
 		if(is_celebrating)
 			PushScreenText();
