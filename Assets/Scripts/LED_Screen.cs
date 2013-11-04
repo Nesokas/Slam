171786d74f0eb5874e13ef50f5c8d2722c9ae818
ines <ines.moital@sherpany.com>
2013-11-04 19:12:48 +0000

    Missing files

---
+++ Assets/Scripts/LED_Screen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LED_Screen : MonoBehaviour {
+	
+	public Texture golden_goal_texture;
+	
+	public void DrawTieMessage()
+	{
+		renderer.material.SetTexture("_MainTex", golden_goal_texture);
+	}
+}
