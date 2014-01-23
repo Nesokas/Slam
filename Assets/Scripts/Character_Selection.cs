d2ed2dd22125bddf8d5346a47e5e1247474b2309
Nesokas <ines.moital@gmail.com>
2014-01-23 00:20:51 +0000

    Bod mesh/bones corrected

---
+++ Assets/Scripts/Character_Selection.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Selection : MonoBehaviour {
+
+	public GameObject[] heroes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
