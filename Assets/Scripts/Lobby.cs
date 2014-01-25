eb531d3c7c5406afa9fa4492445be59207bcafbb
Nesokas <ines.moital@gmail.com>
2014-01-25 18:57:05 +0000

    Trying to solve facilitator problems

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -318,6 +318,12 @@ public class Lobby : MonoBehaviour
 		}
 		/*****************************************************************************************************************/
 		
+		networkView.RPC ("DisableLobby", RPCMode.All);
+	}
+
+	[RPC]
+	void DisableLobby()
+	{
 		show_lobby = false;
 	}
 	
@@ -375,7 +381,8 @@ public class Lobby : MonoBehaviour
 	
 	void ConnectToServer()
 	{
-		show_lobby = false;
+		show_lobby = true;
+		game_settings.connect_to.useNat = true;
 		Network.Connect(game_settings.connect_to.ip, game_settings.connect_to.port);
 	}
 	
@@ -387,7 +394,6 @@ public class Lobby : MonoBehaviour
 	[RPC]
 	void TellName(NetworkPlayer network_player, string new_player_name)
 	{
-		
 		/******* Initialize new player lists *********/
 		for(int i = 0; i < team_1.Count; i++)
 			networkView.RPC("AddNetworkPlayer", network_player, team_1[i].network_player, team_1[i].name, TEAM_1);
@@ -613,6 +619,17 @@ public class Lobby : MonoBehaviour
 	{
 
 	}
+
+	string testStatus = "Testing network connection capabilities.";
+	string testMessage = "Test in progress";
+	string shouldEnableNatMessage = "";
+	bool doneTesting = false;
+	bool probingPublicIP = false;
+	int serverPort = 9999;
+	ConnectionTesterStatus connectionTestResult = ConnectionTesterStatus.Undetermined;
+	bool useNat = false;
+
+	float timer = 0f;
 	
 	void OnGUI()
 	{	
@@ -628,6 +645,100 @@ public class Lobby : MonoBehaviour
 				break;
 			}
 			GUILayout.EndArea();
+		}
+
+		/**************************** UNCOMMENT IF NET TEST NEEDED *******************************/
+//		GUILayout.Label("Current Status: " + testStatus);
+//		GUILayout.Label("Test result : " + testMessage);
+//		GUILayout.Label(shouldEnableNatMessage);
+//		if (!doneTesting)
+//			TestConnection();
+	}
+
+	void TestConnection() {
+		// Start/Poll the connection test, report the results in a label and 
+		// react to the results accordingly
+		connectionTestResult = Network.TestConnection();
+		switch (connectionTestResult) {
+		case ConnectionTesterStatus.Error: 
+			testMessage = "Problem determining NAT capabilities";
+			doneTesting = true;
+			break;
+			
+		case ConnectionTesterStatus.Undetermined: 
+			testMessage = "Undetermined NAT capabilities";
+			doneTesting = false;
+			break;
+			
+		case ConnectionTesterStatus.PublicIPIsConnectable:
+			testMessage = "Directly connectable public IP address.";
+			useNat = false;
+			doneTesting = true;
+			break;
+			
+			// This case is a bit special as we now need to check if we can 
+			// circumvent the blocking by using NAT punchthrough
+		case ConnectionTesterStatus.PublicIPPortBlocked:
+			testMessage = "Non-connectable public IP address (port " +
+				serverPort +" blocked), running a server is impossible.";
+			useNat = false;
+			// If no NAT punchthrough test has been performed on this public 
+			// IP, force a test
+			if (!probingPublicIP) {
+				connectionTestResult = Network.TestConnectionNAT();
+				probingPublicIP = true;
+				testStatus = "Testing if blocked public IP can be circumvented";
+				timer = Time.time + 10;
+			}
+			// NAT punchthrough test was performed but we still get blocked
+			else if (Time.time > timer) {
+				probingPublicIP = false; 		// reset
+				useNat = true;
+				doneTesting = true;
+			}
+			break;
+		case ConnectionTesterStatus.PublicIPNoServerStarted:
+			testMessage = "Public IP address but server not initialized, "+
+				"it must be started to check server accessibility. Restart "+
+					"connection test when ready.";
+			break;
+			
+		case ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted:
+			testMessage = "Limited NAT punchthrough capabilities. Cannot "+
+				"connect to all types of NAT servers. Running a server "+
+					"is ill advised as not everyone can connect.";
+			useNat = true;
+			doneTesting = true;
+			break;
+			
+		case ConnectionTesterStatus.LimitedNATPunchthroughSymmetric:
+			testMessage = "Limited NAT punchthrough capabilities. Cannot "+
+				"connect to all types of NAT servers. Running a server "+
+					"is ill advised as not everyone can connect.";
+			useNat = true;
+			doneTesting = true;
+			break;
+			
+		case ConnectionTesterStatus.NATpunchthroughAddressRestrictedCone:
+		case ConnectionTesterStatus.NATpunchthroughFullCone:
+			testMessage = "NAT punchthrough capable. Can connect to all "+
+				"servers and receive connections from all clients. Enabling "+
+					"NAT punchthrough functionality.";
+			useNat = true;
+			doneTesting = true;
+			break;
+			
+		default: 
+			testMessage = "Error in test routine, got " + connectionTestResult;
+			break;
+		}
+		if (doneTesting) {
+			if (useNat)
+				shouldEnableNatMessage = "When starting a server the NAT "+
+					"punchthrough feature should be enabled (useNat parameter)";
+			else
+				shouldEnableNatMessage = "NAT punchthrough not needed";
+			testStatus = "Done testing";
 		}
 	}
 	
